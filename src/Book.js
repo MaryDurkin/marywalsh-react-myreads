233633c8fd1dd1a833ab837220f04fd6b22ce767
Mary <mary.walsh@gmail.com>
2017-08-30 10:04:06 -0400

    Added requirements and suggestions from reviewer.

--- src/Book.js
+++ src/Book.js
@@ -15,10 +15,7 @@ class Book extends React.Component {
 
   handleChange = (e) => {
     const { book, updateShelf } = this.props
-    let newShelf = e.target.value
-    if (newShelf !== "none") {
-      updateShelf(book, newShelf)
-    }
+    updateShelf(book, e.target.value)
   }
 
   render () {
@@ -31,22 +28,21 @@ class Book extends React.Component {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookCover})` }}></div>
+          <div className="book-cover" style={{ backgroundImage: `url(${bookCover})` }}></div>
           <div className="book-shelf-changer">
-            <select onChange={this.handleChange} value='none'>
-              <option value="none" disabled>Move to...</option>
+            <select onChange={this.handleChange} value={book.shelf}>
+              <option value="moveTo" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
               <option value="read">Read</option>
-              <option value="remove">None</option>
+              <option value="none">None</option>
             </select>
           </div>
         </div>
         <div className="book-title">{book.title}</div>
         <div className="book-authors">
-          { book.authors && (book.authors.map(author =>
-            <div key={author}>{author}</div>
-          ))}
+          {book.authors ? book.authors.join(',') : ''}
+
         </div>
       </div>
     )
@@ -54,3 +50,7 @@ class Book extends React.Component {
 }
 
 export default Book
+/*   { book.authors && (book.authors.map(author =>
+    <div key={author}>{author}</div>
+  ))}
+  */
